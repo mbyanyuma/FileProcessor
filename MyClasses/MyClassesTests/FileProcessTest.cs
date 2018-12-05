b730f5ec1dddde73180d8c19449510a23c4b435b
Moses Byanyuma <mbyanyuma108@gmail.com>
2018-12-05 17:55:47 -0500

    Added unit tests for FileProcess class. 3 test approaches included: - happy path - sad path - testing for null or empty string

---
+++ MyClasses/MyClassesTests/FileProcessTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+
+
+namespace MyClassesTests
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        //constants
+        private readonly string badFileName = @"c:/marketwatch.stocks";
+
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+            //Arrange
+            FileProcess fp = new FileProcess();
+
+            //Act
+            var actual = fp.FileExists(@"c:/users/mbyan/documents/sampleTextFiles/testFile.txt");
+
+            //Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            //Arrange
+            FileProcess fp = new FileProcess();
+
+            //Act
+            var actual = fp.FileExists(badFileName);
+
+            //Assert
+            Assert.IsFalse(actual);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            //Arrange
+            FileProcess fp = new FileProcess();
+            
+
+            //Act
+             fp.FileExists("");
+
+            //Assert
+            
+        }
+    }
+}
