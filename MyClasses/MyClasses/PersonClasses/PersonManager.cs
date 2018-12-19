1d9ba1584912d20626de64b7dc594927f730753c
Moses Byanyuma <mbyanyuma108@gmail.com>
2018-12-19 15:14:52 -0500

    - added data-driven test - made use of more assert classes

---
+++ MyClasses/MyClasses/PersonClasses/PersonManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClasses.PersonClasses
+{
+    public class PersonManager
+    {
+
+        public Person CreatePerson(string first, string last, bool isSupervisor)
+        {
+            Person person = null;
+
+            if (!string.IsNullOrEmpty(first))
+            {
+                if (isSupervisor)
+                {
+                    person = new Supervisor();
+                }
+                else
+                {
+                    person = new Employee();
+                }
+
+
+                //assign variables
+                person.FirstName = first;
+                person.LastName = last;
+
+            }
+
+            return person;
+        }
+
+
+    }
+}
